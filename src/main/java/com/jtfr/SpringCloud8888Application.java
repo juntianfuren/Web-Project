b49e2030de23e5619d8cf6afd55b7856e896d3c2
ckm <1123181523@qq.com>
2020-09-21 22:45:11 +0800

    提交代码

--- src/main/java/com/jtfr/SpringCloud8888Application.java
+++ src/main/java/com/jtfr/SpringCloud8888Application.java
@@ -2,7 +2,6 @@ package com.jtfr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -13,13 +12,4 @@ public class SpringCloud8888Application {
 		SpringApplication.run(SpringCloud8888Application.class, args);
 	}
 
-	@GetMapping("/test")
-	public String test() {
-		return "com.jtfr.Application.test";
-	}
-	
-	@GetMapping()
-	public String test2() {
-		return "com.jtfr.Application+test2";
-	}
 }
